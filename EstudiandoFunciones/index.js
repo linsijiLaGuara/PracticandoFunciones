957229524e7ef93168d0274a2efc16a7e70c7448
Linsiji <42350425+linsijiLaGuara@users.noreply.github.com>
2024-01-22 19:23:30 -0300

    cerrando repaso

---
+++ EstudiandoFunciones/index.js
@@ -0,0 +1,18 @@
+//template string
+
+const numero = (num) => {
+
+    return 'El numero es:' + num
+}
+const resultado = numero(10)
+console.log(resultado)
+
+
+//mescla de string
+const numero_1 = (num) => {
+
+return +`el numero es:${num}`  
+}
+const resultado_1 = numero_1(10)
+console.log(resultado)
+
---
+++ EstudiandoFunciones/objetos.js
@@ -0,0 +1,13 @@
+//definir objetos 
+
+ const mascota ={
+    nombre:'tom',
+    edad: 10,
+    vivo: true
+ }
+ console.log(mascota)
+ //acceder a la propiedad 
+ console.log(mascota.nombre)
+
+
+// destructuring objects
